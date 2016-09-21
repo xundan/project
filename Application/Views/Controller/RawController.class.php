47952637fa563fc0306d33eb7dd9f366f4b66f92
xuncl <xuncl@126.com>
2016-09-21 12:12:37 +0800

    make content editable while auditing
    RV by lx

--- Application/Views/Controller/RawController.class.php
+++ Application/Views/Controller/RawController.class.php
@@ -7,37 +7,41 @@
  */
 
 namespace Views\Controller;
+
 use Think\Controller\RestController;
+use Think\Log;
 
 class RawController extends RestController
 {
-    protected $allowMethod    = array('get','post','put'); // REST允许的请求类型列表
-    protected $allowType      = array('json'); // REST允许请求的资源类型列表
+    protected $allowMethod = array('get', 'post', 'put'); // REST允许的请求类型列表
+    protected $allowType = array('json'); // REST允许请求的资源类型列表
 
 
-
-    public function index(){
+    public function index()
+    {
 //        $this->createRaw("test");
         echo "Raw api works";
     }
 
-    public function demo(){
+    public function demo()
+    {
         $this->listRaws();
     }
 
-    Public function messages() {
+    Public function messages()
+    {
         $data = array(
-            "result_code"=>"105",
-            "reason"=>"应用未审核超时，请提交认证",
-            "result"=>null,
-            "message_id"=>null,
-            "error_code"=>10005,
+            "result_code" => "105",
+            "reason" => "应用未审核超时，请提交认证",
+            "result" => null,
+            "message_id" => null,
+            "error_code" => 10005,
         );
-        switch ($this->_method){
+        switch ($this->_method) {
             case 'get': // get请求处理代码
-                if ($this->_type == 'html'){
+                if ($this->_type == 'html') {
                     echo 'html';
-                }elseif($this->_type == 'xml'){
+                } elseif ($this->_type == 'xml') {
                     echo 'xml';
                 }
                 echo '<br>restful url is correct.';
@@ -49,76 +53,76 @@ class RawController extends RestController
                 break;
             case 'post': // post请求处理代码
 
-                $result1 = $GLOBALS['HTTP_RAW_POST_DATA'] ;
+                $result1 = $GLOBALS['HTTP_RAW_POST_DATA'];
                 $object2 = json_decode($result1, true);
 
-                if ($object2){
-                    $content=$object2['content'];
-                    $sender="";
-                    $sender_wx="";
+                if ($object2) {
+                    $content = $object2['content'];
+                    $sender = "";
+                    $sender_wx = "";
                     date_default_timezone_set('PRC');
-                    $title = date('y-m-d_h:i',time());
+                    $title = date('y-m-d_h:i', time());
                     $pattern = '/@\s*(.+)\s*\#\s*(.+)\s*\$([\s\S]*[0-9]{11}[\s\S]*)/';
-                    $res=preg_match($pattern,$content,$match);
-                    $owner=null;
+                    $res = preg_match($pattern, $content, $match);
+                    $owner = null;
                     if ($res) {
-                        $owner=$match[1];
-                        $sender_wx=$match[2];
-                        $sender=$sender_wx;
-                        $content=$match[3];
+                        $owner = $match[1];
+                        $sender_wx = $match[2];
+                        $sender = $sender_wx;
+                        $content = $match[3];
                     } else {
                         // TODO 不符合格式的消息 通知开发检查手机端过滤逻辑
-                        $data['result_code']="301";
-                        $data['reason']="数据格式不合法，本地过滤失效";
-                        $data['error_code']="10301";
-                        $data['message_id']=$title;
-                        $data['result']=$object2['content'];
+                        $data['result_code'] = "301";
+                        $data['reason'] = "数据格式不合法，本地过滤失效";
+                        $data['error_code'] = "10301";
+                        $data['message_id'] = $title;
+                        $data['result'] = $object2['content'];
                     }
-                    $title.=substr($content,0,26);
+                    $title .= substr($content, 0, 26);
 
                     $insert = $this->createRaw($title, $content, $owner, $sender, $sender_wx);
-                    if($insert){
-                        $data['result_code']="201";
-                        $data['reason']="新建或修改数据成功";
-                        $data['error_code']="0";
-                        $data['message_id']=$title;
-                        $data['result']=$object2['content'];
-                    }else{
+                    if ($insert) {
+                        $data['result_code'] = "201";
+                        $data['reason'] = "新建或修改数据成功";
+                        $data['error_code'] = "0";
+                        $data['message_id'] = $title;
+                        $data['result'] = $object2['content'];
+                    } else {
                         // TODO 插入失败，通知开发人员
-                        $data['result_code']="106";
-                        $data['reason']="数据库操作错误";
-                        $data['error_code']="10006";
-                        $data['message_id']=$title;
-                        $data['result']=$object2['content'];
+                        $data['result_code'] = "106";
+                        $data['reason'] = "数据库操作错误";
+                        $data['error_code'] = "10006";
+                        $data['message_id'] = $title;
+                        $data['result'] = $object2['content'];
                     }
-                }else{
+                } else {
                     //TODO 数据为空，网络错误，客户端错误，通知开发人员
-                    $data['result_code']="500";
-                    $data['reason']="内部错误";
-                    $data['message_id']=null;
-                    $data['error_code']="10500";
-                    $data['result']=null;
+                    $data['result_code'] = "500";
+                    $data['reason'] = "内部错误";
+                    $data['message_id'] = null;
+                    $data['error_code'] = "10500";
+                    $data['result'] = null;
                 }
 
-                $this->response($data,'json');
+                $this->response($data, 'json');
                 break;
         }
     }
 
-    // TODO targets
-    Public function targets() {
+    Public function targets()
+    {
         $data = array(
-            "result_code"=>"105",
-            "reason"=>"应用未审核超时，请提交认证",
-            "result"=>null,
-            "message_id"=>null,
-            "error_code"=>10005,
+            "result_code" => "105",
+            "reason" => "应用未审核超时，请提交认证",
+            "result" => null,
+            "message_id" => null,
+            "error_code" => 10005,
         );
-        switch ($this->_method){
+        switch ($this->_method) {
             case 'get': // get请求处理代码
-                if ($this->_type == 'html'){
+                if ($this->_type == 'html') {
                     echo 'html';
-                }elseif($this->_type == 'xml'){
+                } elseif ($this->_type == 'xml') {
                     echo 'xml';
                 }
                 echo '<br>restful url is correct.';
@@ -130,14 +134,14 @@ class RawController extends RestController
                 break;
             case 'post': // post请求处理代码
 
-                $result1 = $GLOBALS['HTTP_RAW_POST_DATA'] ;
+                $result1 = $GLOBALS['HTTP_RAW_POST_DATA'];
                 $object2 = json_decode($result1, true);
 
-                if ($object2){
+                if ($object2) {
                     $mdate = $object2['mdate'];
                     $name = $object2['name'];
-                    // TODO 只对两天外的数据查重，对两天内的数据更新
-                    if ($this->notExistTarget($mdate,$name)) {
+
+                    if ($this->notExistTarget($mdate, $name)) {
                         $endtime = $object2['endtime'];
                         $mmaxvalue = $object2['mmaxvalue'];
                         $starttime = $object2['starttime'];
@@ -152,43 +156,73 @@ class RawController extends RestController
                             , $isagenda, $isdone, $mvalue, $minterval, $todayvalue);
                         if ($insert) {
                             $data['result_code'] = "201";
-                            $data['reason'] = "新建或修改数据成功";
+                            $data['reason'] = "新建数据成功";
                             $data['error_code'] = "0";
                             $data['message_id'] = $name . $mdate;
-                            $data['result'] = $object2['content'];
+                            $data['result'] = $name . $mdate;
                         } else {
-                            // TODO 插入失败，通知开发人员
                             $data['result_code'] = "106";
                             $data['reason'] = "数据库操作错误";
                             $data['error_code'] = "10006";
-                            $data['message_id'] = $name;
-                            $data['result'] = $object2['content'];
+                            $data['message_id'] = $name . $mdate;
+                            $data['result'] = $name . $mdate;
+                            Log::record("insert target error, $name $mdate", Log::ERR);
+                        }
+                    } else {
+                        $yesterdayvalue = $object2['yesterdayvalue'];
+                        $description = $object2['description'];
+                        $isdone = $object2['isdone'];
+                        $mvalue = $object2['mvalue'];
+                        $todayvalue = $object2['todayvalue'];
+                        $update = $this->updateTarget($name, $mdate, $mvalue, $isdone,
+                            $description, $todayvalue, $yesterdayvalue);
+                        if ($update) {
+                            $data['result_code'] = "201";
+                            $data['reason'] = "修改数据成功";
+                            $data['error_code'] = "0";
+                            $data['message_id'] = $name . $mdate;
+                            $data['result'] = $name . $mdate;
+                        } elseif ($update===0){
+                            $data['result_code'] = "202";
+                            $data['reason'] = "数据库已是最新";
+                            $data['error_code'] = "0";
+                            $data['message_id'] = $name . $mdate;
+                            $data['result'] = $name . $mdate;
+                        }else{
+                            $data['result_code'] = "106";
+                            $data['reason'] = "数据库操作错误";
+                            $data['error_code'] = "10006";
+                            $data['message_id'] = $name . $mdate;
+                            $data['result'] = $name . $mdate;
+                            Log::record("update target error, $name $mdate", Log::ERR);
                         }
                     }
-                }else{
-                    //TODO 数据为空，网络错误，客户端错误，通知开发人员
-                    $data['result_code']="500";
-                    $data['reason']="内部错误";
-                    $data['message_id']=null;
-                    $data['error_code']="10500";
-                    $data['result']=null;
+                } else {
+                    $data['result_code'] = "500";
+                    $data['reason'] = "内部错误";
+                    $data['message_id'] = null;
+                    $data['error_code'] = "10500";
+                    $data['result'] = null;
                 }
 
-                $this->response($data,'json');
+                $this->response($data, 'json');
                 break;
         }
     }
 
     //查重
-    private function notExistTarget($date, $name){
+    private function notExistTarget($date, $name)
+    {
         $result = true;
-        if (D('Targets')->where("mdate = '$date' and name = '$name''")->find()){
+        if (D('Targets')->where("mdate = '$date' and name = '$name'")->find()) {
             $result = false;
+            Log::record("Target exists: $date $name", Log::WARN);
         }
         return $result;
     }
 
-    private function listRaws(){
+    private function listRaws()
+    {
         dump(D('Raw')->select());
     }
 
@@ -204,7 +238,7 @@ class RawController extends RestController
     {
         //解决短时间内重复调用问题
         $duplicate_data = D('Raw')->where("rid = '$title'")->find();
-        if($duplicate_data){
+        if ($duplicate_data) {
             return $duplicate_data;
         }
 
@@ -214,39 +248,54 @@ class RawController extends RestController
             'sender' => $sender,
             'type' => 'plain',
             'remark' => '0',
-            'status'=>0,
+            'status' => 0,
             'owner' => $owner,
-            'sender_wx'=>$sender_wx,
+            'sender_wx' => $sender_wx,
         );
         $insert = D('Raw')->add($rawAttribute);
         return $insert;
     }
 
     public function createTarget($name, $date, $starttime, $endtime, $mmaxvalue,
-        $yesterdayvalue, $description, $isagenda, $isdone, $mvalue, $minterval, $todayvalue)
+                                 $yesterdayvalue, $description, $isagenda, $isdone, $mvalue, $minterval, $todayvalue)
     {
         $rawAttribute = array(
-            "name"=>$name,
-            "mdate"=>$date,
-            "starttime"=>$starttime,
-            "endtime"=>$endtime,
-            "description"=>$description,
-            "mvalue"=>$mvalue,
-            "isagenda"=>$isagenda,
-            "minterval"=>$minterval,
-            "mmaxvalue"=>$mmaxvalue,
-            "todayvalue"=>$todayvalue,
-            "yesterdayvalue"=>$yesterdayvalue,
-            "isdone"=>$isdone,
+            "name" => $name,
+            "mdate" => $date,
+            "starttime" => $starttime,
+            "endtime" => $endtime,
+            "description" => $description,
+            "mvalue" => $mvalue,
+            "isagenda" => $isagenda,
+            "minterval" => $minterval,
+            "mmaxvalue" => $mmaxvalue,
+            "todayvalue" => $todayvalue,
+            "yesterdayvalue" => $yesterdayvalue,
+            "isdone" => $isdone,
         );
         $insert = D('Targets')->add($rawAttribute);
         return $insert;
     }
 
-    private function remarkRaw($rawId){
+    public function updateTarget($name, $mdate, $mvalue, $isdone,
+                                 $description, $todayvalue, $yesterdayvalue)
+    {
+        $rawAttribute = array(
+            "description" => $description,
+            "mvalue" => $mvalue,
+            "todayvalue" => $todayvalue,
+            "yesterdayvalue" => $yesterdayvalue,
+            "isdone" => $isdone,
+        );
+        $update = D('Targets')->where("mdate = '$mdate' and name = '$name'")->save($rawAttribute);
+        return $update;
+    }
+
+    private function remarkRaw($rawId)
+    {
         $rawUpdateAttribute = array(
-            'id'=>$rawId,
-            'remark'=>'1',
+            'id' => $rawId,
+            'remark' => '1',
         );
         D('Raw')->save($rawUpdateAttribute);
     }
@@ -255,7 +304,8 @@ class RawController extends RestController
 //        D('Raw')->delete($rawId);
 //    }
 
-    private function showRaw($rawId){
+    private function showRaw($rawId)
+    {
         dump(D('Raw')->find($rawId));
     }
 }
